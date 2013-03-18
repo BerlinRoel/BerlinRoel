034803d5d53f119978c87ae95926c43520ea5383
Berlin <tplberzbay@gmail.com>
2013-03-18 21:02:01 +0800

    17 files

--- Arrow website/mailer-contact.php
+++ Arrow website/mailer-contact.php
@@ -1,6 +1,6 @@
 <?PHP 
 
-$to = "tplberzbay@gmail.com; roelmargarejo@gmail.com";
+$to = "tplberzbay@gmail.com; cokarlene@gmail.com";
 //$to = "giselle.saldaga@gmail.com";
 $subject = "ARROW";
 $headers = "From: Form Mailer";
